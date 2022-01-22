ca1a1ce5ad654b2cb708ee953645c840670e28c8
norton16 <bnorton184@gmail.com>
2022-01-22 12:56:51 -0600

    Updates

--- src/mock/data.js
+++ src/mock/data.js
@@ -11,16 +11,19 @@ export const headData = {
 export const heroData = {
   title: 'Hello! My name is ',
   name: 'Brian.',
-  subtitle: 'I am a Web Developer.',
-  cta: 'Know more',
+  subtitle: 'I am a Frontend Developer.',
+  cta: 'About me',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: "Hello! My name is Brian. I'm from Illinois, and I am a graduate of Purdue University.",
-  paragraphTwo: "Front-End Development is my strength, and I have experience building websites in both professional settings and on my own.",
-  paragraphThree: "When I'm not making websites, I like to hang out with friends, watch NBA games, and play video games.",
+  paragraphOne:
+    "Hello! My name is Brian. I'm from Illinois, and I am a graduate of Purdue University.",
+  paragraphTwo:
+    'Front end development is my strength, and I have experience building websites in both professional settings and on my own.',
+  paragraphThree:
+    "I'm always learning new things, and my goal is to become a fullstack developer in the future.",
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
@@ -30,7 +33,8 @@ export const projectsData = [
     id: nanoid(),
     img: 'sfp.jpg',
     title: 'SFP',
-    info: 'During my 8 months at Stormfront Productions in Lafayette, I learned a ton about the fundamentals of creating professional websites for clients. I picked up experience with Laravel and WordPress as well. Here are a few sites I created with the amazing team there.',
+    info:
+      'During my 8 months at Stormfront Productions in Lafayette, I learned a ton about the fundamentals of creating professional websites for clients. I picked up experience with Laravel and WordPress as well. Here are a few sites I created with the amazing team there.',
     info2: '',
     url: 'https://sfp.net',
     repo: '', // if no repo, the button will not show up
@@ -39,7 +43,8 @@ export const projectsData = [
     id: nanoid(),
     img: 'commerce.jpg',
     title: 'Commerce',
-    info: 'Commerce is an e-commerce site built with React. This project uses the Commerce.js API, Stripe, and Material-UI to deliver a fully functional online shop. This was a great way to practice React Hooks, Router, and JavaScript skills.',
+    info:
+      'Commerce is an e-commerce site built with React. This project uses the Commerce.js API, Stripe, and Material-UI to deliver a fully functional online shop. This was a great way to practice React Hooks, Router, and JavaScript skills.',
     info2: '',
     url: 'https://bnorton184commerce.netlify.app/',
     repo: 'https://github.com/norton16/ecommerce', // if no repo, the button will not show up
@@ -48,7 +53,8 @@ export const projectsData = [
     id: nanoid(),
     img: 'social.png',
     title: 'Social Media',
-    info: "This photo sharing social media website was made with React and Google Firebase. A user can sign in, upload photos with captions, comment on, and delete posts. While creating this project, I learned how to work with Firebase Firestore along with React Hooks and Context API.",
+    info:
+      'This photo sharing social media website was made with React and Google Firebase. A user can sign in, upload photos with captions, comment on, and delete posts. While creating this project, I learned how to work with Firebase Firestore along with React Hooks and Context API.',
     info2: '',
     url: 'https://socialmedia-1f4ae.web.app/',
     repo: 'https://github.com/norton16/simplesocialmedia', // if no repo, the button will not show up
